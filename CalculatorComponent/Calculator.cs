e5bf9aed5bc804a16ad38bdaa0ca44278656534b
andersbor <anbo@easj.dk>
2017-09-24 21:05:33 +0200

    Add project files.

---
+++ CalculatorComponent/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CalculatorComponent
+{
+    public class Calculator
+    {
+       /// <summary>
+       /// Adds to integers
+       /// </summary>
+       /// <param name="a">first number</param>
+       /// <param name="b">second number</param>
+       /// <returns>The sum of the two numbers</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Divides two integers
+        /// </summary>
+        /// <param name="a">Numerator</param>
+        /// <param name="b">Denominator</param>
+        /// <returns>numerator / denomerator</returns>
+        /// <exception cref="DivideByZeroException">if denominator is 0</exception>
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Divides two doubles
+        /// </summary>
+        /// <param name="a">Numerator</param>
+        /// <param name="b">Denominator</param>
+        /// <returns>numerator / denomerator</br>
+        /// Double.Infinity if denumerator is 0.0
+        /// </returns>
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ CalculatorComponentTests/CalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorComponent.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private static readonly double Delta = 0.000001;
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.AreEqual(9, Calculator.Add(4, 5));
+            Assert.AreEqual(9.0, Calculator.Add(4.0, 5.0), Delta);
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            Assert.AreEqual(2, Calculator.Subtract(7, 5));
+            Assert.AreEqual(2.1, Calculator.Subtract(7.1, 5.0), Delta);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            Assert.AreEqual(10, Calculator.Multiply(2, 5));
+            Assert.AreEqual(4.2, Calculator.Multiply(2.1, 2.0), Delta);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            Assert.AreEqual(3, Calculator.Divide(7, 2));
+            try
+            {
+                Calculator.Divide(7, 0);
+                Assert.Fail();
+            }
+            catch (DivideByZeroException) { }
+
+            Assert.AreEqual(3.5, Calculator.Divide(7.0, 2.0), Delta);
+            Assert.AreEqual(double.PositiveInfinity, Calculator.Divide(4.0, 0.0));
+        }
+    }
+}
